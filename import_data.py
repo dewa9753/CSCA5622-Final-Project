8a7773c1a06a44f3b88998d2b95121b3c9e9f958
dewa9753 <dewa9753@colorado.edu>
2025-10-16 16:02:03 -0600

    changed data directory structure

--- import_data.py
+++ import_data.py
@@ -1,6 +1,6 @@
 import kagglehub
 
 # Download latest version
-path = kagglehub.dataset_download("jtrotman/formula-1-race-data")
+path = kagglehub.dataset_download("jtrotman/formula-1-race-data", unzip=True)
 
 print("Path to dataset files:", path)
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from csca5622-final-project!")
-
-
-if __name__ == "__main__":
-    main()
