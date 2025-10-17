36df10c9312041a11b05f5e097e501790e67aa31
dewa9753 <dewa9753@colorado.edu>
2025-10-17 04:54:12 -0600

    change name from globals to settings

--- clean_data.py
+++ clean_data.py
@@ -2,19 +2,19 @@ import pandas as pd
 import numpy as np
 import os
 import sys
-import globals
+import settings
 
 ## functions
 def get_original_data(file_name):
-    df = pd.read_csv(globals.ORIGINAL_DATA_ROOT + file_name)
+    df = pd.read_csv(settings.ORIGINAL_DATA_ROOT + file_name)
     df.replace(to_replace=r'\N', value=np.nan, inplace=True)
     return df
 
 def create_clean_data(df, columns_to_drop, output_file_name):
     df_cleaned = df.drop(columns_to_drop, axis=1)
     df_cleaned = df_cleaned.dropna()
-    df_cleaned.to_csv(globals.CLEAN_DATA_ROOT + output_file_name, index=False)
-    print(f'Cleaned {output_file_name} and created {globals.CLEAN_DATA_ROOT + output_file_name}')
+    df_cleaned.to_csv(settings.CLEAN_DATA_ROOT + output_file_name, index=False)
+    print(f'Cleaned {output_file_name} and created {settings.CLEAN_DATA_ROOT + output_file_name}')
 
 ## clean data if not already cleaned
 if __name__ == '__main__':
@@ -22,16 +22,16 @@ if __name__ == '__main__':
     # check for --force argument to re-clean data
     if len(sys.argv) > 1:
         if sys.argv[1] == '--force':
-            if os.path.exists(globals.CLEAN_DATA_ROOT):
+            if os.path.exists(settings.CLEAN_DATA_ROOT):
                 import shutil
-                shutil.rmtree(globals.CLEAN_DATA_ROOT)
-                print(f'Force cleaning: removed \'{globals.CLEAN_DATA_ROOT}\' directory to re-clean the data.')
+                shutil.rmtree(settings.CLEAN_DATA_ROOT)
+                print(f'Force cleaning: removed \'{settings.CLEAN_DATA_ROOT}\' directory to re-clean the data.')
 
     # create clean root directory if it doesn't exist
-    if not os.path.exists(globals.CLEAN_DATA_ROOT):
-        os.makedirs(globals.CLEAN_DATA_ROOT)
+    if not os.path.exists(settings.CLEAN_DATA_ROOT):
+        os.makedirs(settings.CLEAN_DATA_ROOT)
 
-    if not os.path.exists(globals.CLEAN_DATA_ROOT + 'circuits_clean.csv'):
+    if not os.path.exists(settings.CLEAN_DATA_ROOT + 'circuits_clean.csv'):
         df = get_original_data('circuits.csv')
         create_clean_data(
             df,
@@ -39,7 +39,7 @@ if __name__ == '__main__':
             'circuits_clean.csv'
         )
     
-    if not os.path.exists(globals.CLEAN_DATA_ROOT + 'constructor_results_clean.csv'):
+    if not os.path.exists(settings.CLEAN_DATA_ROOT + 'constructor_results_clean.csv'):
         df = get_original_data('constructor_results.csv')
         create_clean_data(    
             df,
@@ -47,7 +47,7 @@ if __name__ == '__main__':
             'constructor_results_clean.csv'
         )
     
-    if not os.path.exists(globals.CLEAN_DATA_ROOT + 'constructor_standings_clean.csv'):
+    if not os.path.exists(settings.CLEAN_DATA_ROOT + 'constructor_standings_clean.csv'):
         df = get_original_data('constructor_standings.csv')
         create_clean_data(
             df,
@@ -55,7 +55,7 @@ if __name__ == '__main__':
             'constructor_standings_clean.csv'
         )
     
-    if not os.path.exists(globals.CLEAN_DATA_ROOT + 'constructors_clean.csv'):
+    if not os.path.exists(settings.CLEAN_DATA_ROOT + 'constructors_clean.csv'):
         df = get_original_data('constructors.csv')
         create_clean_data(
             df,
@@ -63,7 +63,7 @@ if __name__ == '__main__':
             'constructors_clean.csv'
         )
     
-    if not os.path.exists(globals.CLEAN_DATA_ROOT + 'drivers_standings_clean.csv'):
+    if not os.path.exists(settings.CLEAN_DATA_ROOT + 'drivers_standings_clean.csv'):
         df = get_original_data('driver_standings.csv')
         create_clean_data(
             df,
@@ -71,7 +71,7 @@ if __name__ == '__main__':
             'drivers_standings_clean.csv'
         )
 
-    if not os.path.exists(globals.CLEAN_DATA_ROOT + 'drivers_clean.csv'):
+    if not os.path.exists(settings.CLEAN_DATA_ROOT + 'drivers_clean.csv'):
         df = get_original_data('drivers.csv')
         create_clean_data(
             df,
@@ -79,7 +79,7 @@ if __name__ == '__main__':
             'drivers_clean.csv'
         )
 
-    if not os.path.exists(globals.CLEAN_DATA_ROOT + 'lap_times_clean.csv'):
+    if not os.path.exists(settings.CLEAN_DATA_ROOT + 'lap_times_clean.csv'):
         df = get_original_data('lap_times.csv')
         create_clean_data(
             df,
@@ -87,7 +87,7 @@ if __name__ == '__main__':
             'lap_times_clean.csv'
         )
     
-    if not os.path.exists(globals.CLEAN_DATA_ROOT + 'pit_stops_clean.csv'):
+    if not os.path.exists(settings.CLEAN_DATA_ROOT + 'pit_stops_clean.csv'):
         df = get_original_data('pit_stops.csv')
         create_clean_data(
             df,
@@ -95,7 +95,7 @@ if __name__ == '__main__':
             'pit_stops_clean.csv'
         )
 
-    if not os.path.exists(globals.CLEAN_DATA_ROOT + 'qualifying_clean.csv'):
+    if not os.path.exists(settings.CLEAN_DATA_ROOT + 'qualifying_clean.csv'):
         df = get_original_data('qualifying.csv')
         create_clean_data(
             df,
@@ -103,7 +103,7 @@ if __name__ == '__main__':
             'qualifying_clean.csv'
     )
         
-    if not os.path.exists(globals.CLEAN_DATA_ROOT + 'races_clean.csv'):
+    if not os.path.exists(settings.CLEAN_DATA_ROOT + 'races_clean.csv'):
         df = get_original_data('races.csv')
         create_clean_data(
             df,
@@ -111,7 +111,7 @@ if __name__ == '__main__':
             'races_clean.csv'
         )
 
-    if not os.path.exists(globals.CLEAN_DATA_ROOT + 'results_clean.csv'):
+    if not os.path.exists(settings.CLEAN_DATA_ROOT + 'results_clean.csv'):
         df = get_original_data('results.csv')
         create_clean_data(
             df,
@@ -119,7 +119,7 @@ if __name__ == '__main__':
             'results_clean.csv'
         )
 
-    if not os.path.exists(globals.CLEAN_DATA_ROOT + 'sprint_results_clean.csv'):
+    if not os.path.exists(settings.CLEAN_DATA_ROOT + 'sprint_results_clean.csv'):
         df = get_original_data('sprint_results.csv')
         create_clean_data(
             df,
@@ -127,7 +127,7 @@ if __name__ == '__main__':
             'sprint_results_clean.csv'
         )
 
-    if not os.path.exists(globals.CLEAN_DATA_ROOT + 'status_clean.csv'):
+    if not os.path.exists(settings.CLEAN_DATA_ROOT + 'status_clean.csv'):
         df = get_original_data('status.csv')
         create_clean_data(
             df,
--- import_data.py
+++ import_data.py
@@ -2,20 +2,20 @@ import kagglehub
 import os
 import sys
 import shutil
-import globals
+import settings
 from pathlib import Path
 
 if len(sys.argv) > 1 and sys.argv[1] == '--force':
     if os.path.exists('data_original'):
         shutil.rmtree('data_original')
-        print(f"Force re-downloading: removed '{globals.ORIGINAL_DATA_ROOT}' directory to re-download the data.")
+        print(f"Force re-downloading: removed '{settings.ORIGINAL_DATA_ROOT}' directory to re-download the data.")
 
 # Download latest version
 path = kagglehub.dataset_download("jtrotman/formula-1-race-data")
 
 try:
     shutil.move(path + '\\', './')
-    Path('104').rename(globals.ORIGINAL_DATA_ROOT)
-    print(f"Successfully downloaded data to '{globals.ORIGINAL_DATA_ROOT}'!")
+    Path('104').rename(settings.ORIGINAL_DATA_ROOT)
+    print(f"Successfully downloaded data to '{settings.ORIGINAL_DATA_ROOT}'!")
 except Exception as e:
-    print(f"Error moving downloaded data to '{globals.ORIGINAL_DATA_ROOT}': {e}")
+    print(f"Error moving downloaded data to '{settings.ORIGINAL_DATA_ROOT}': {e}")
